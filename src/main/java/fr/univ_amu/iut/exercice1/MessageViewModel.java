5aa55d3c20e8ccefc883d9b1461bf184bd69282f
noe.petit <nonoti069@gmail.com>
2026-06-01 06:52:28 +0000

    feat (exercice1): exercice Terminé

--- src/main/java/fr/univ_amu/iut/exercice1/MessageViewModel.java
+++ src/main/java/fr/univ_amu/iut/exercice1/MessageViewModel.java
@@ -1,5 +1,6 @@
 package fr.univ_amu.iut.exercice1;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -35,11 +36,16 @@ public class MessageViewModel {
     //
     // 1. Initialiser la propriété `texte` avec la valeur actuelle du modèle
     //    (message.getTexte()).
+    texte.set(message.getTexte());
+    texte.addListener((obs, old, newVal) -> message.setTexte(newVal));
+    apercu.bind(Bindings.concat("Aperçu : ", texte));
     // 2. Quand `texte` change, recopier la nouvelle valeur dans le modèle
     //    (message.setTexte(...)) : c'est ce qui garde le modèle à jour.
+
     // 3. Lier `apercu` (lecture seule pour la vue) à une version dérivée de
     //    `texte` : le texte saisi précédé de la mention "Aperçu : ".
     //    Astuce : Bindings.concat("Aperçu : ", texte).
+
   }
 
   public StringProperty texteProperty() {
